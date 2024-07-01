a4c4cba41e22a49622af1e83b06bc5f8e00c396c
Alex Junior <alexjunior.dev@gmail.com>
2024-07-01 10:24:51 -0300

    comando delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -103,7 +103,7 @@ class Usuario {
         if (count($results) > 0) {
             $this->setData($results[0]);
         }
-        
+
     }
 
     public function update($login, $password) {
@@ -118,6 +118,18 @@ class Usuario {
             ':ID'=> $this->getIdusuario()
         ));
 
+    }
+
+    public function delete() {
+        $sql = new Sql();
+        $sql -> query('DELETE FROM tb_usuarios WHERE idusuario = :ID', array(
+            ':ID'=>$this->getIdusuario(),
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
     }
 
     public function __construct($login = "", $password = "") {
--- index.php
+++ index.php
@@ -2,35 +2,38 @@
 
 require_once("config.php");
 
-//Carrega um usuário
-// $root = new Usuario();
-// $root->loadById(3);
-// echo $root;
+/*Carrega um usuário
+ $root = new Usuario();
+ $root->loadById(3);
+ echo $root;*/
 
-//Carrega uma lista de usuários
-// $lista = Usuario::getList();
-// echo json_encode($lista);
+/*Carrega uma lista de usuários
+ $lista = Usuario::getList();
+ echo json_encode($lista);*/
 
-//Carrega uma lista de usuários buscando pelo login
-// $search = Usuario::search("jo");
-// echo json_encode($search);
+/*Carrega uma lista de usuários buscando pelo login
+ $search = Usuario::search("jo");
+ echo json_encode($search);*/
 
-//Carrega um usuario usando o login e a senha
-// $usuario = new Usuario();
-// $usuario->login("root","!@#$");
-// echo $usuario;
+/*Carrega um usuario usando o login e a senha
+ $usuario = new Usuario();
+ $usuario->login("root","!@#$");
+ echo $usuario;*/
 
-//Criando um novo usuário
-// $aluno = new Usuario("aluno", "@alun0");
-// $aluno->insert();
-// echo $aluno;
+/*Criando um novo usuário
+ $aluno = new Usuario("aluno", "@alun0");
+ $aluno->insert();
+ echo $aluno;*/
+
+/*Alterar umm usuário
+ $usuario = new Usuario();
+ $usuario->loadById(3);
+ $usuario->update("professor", "$!@#$");
+ echo $usuario;*/
 
 $usuario = new Usuario();
-
 $usuario->loadById(3);
-
-$usuario->update("professor", "$!@#$");
-
+$usuario->delete();
 echo $usuario;
 
 
